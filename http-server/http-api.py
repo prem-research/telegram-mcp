be5baec7013e39a7a4dfa56a1fd237122b0533e0
Federico <65908512+coval3nte@users.noreply.github.com>
2025-04-02 20:57:16 +0200

    fix

--- http-server/http-api.py
+++ http-server/http-api.py
@@ -1,21 +1,29 @@
 from telethon.hints import Entity
-
 from telegram import client, me as myself
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
 
+'''
 async def telegram_connection():
     if not client.is_connected():
         await client.connect()
         print(await client.get_me())
+'''
+
+
+async def format_entity(entity: Entity) -> dict:
+    return entity.to_dict()
+
 
 @app.get('/get_unread_chats')
 async def get_unread_chats():
     chats = await get_chats()
     return {k: v for k, v in chats.items() if v['unread_count'] > 0}
+
 
 @app.get('/get_unread_messages/{chat_id}')
 async def get_unread_messages(chat_id: int):
@@ -31,22 +39,13 @@ async def get_unread_messages(chat_id: int):
 
     return await get_messages(chat_id=chat_id, count=unread_count)
 
-async def format_entity(entity: Entity) -> dict:
-    return entity.to_dict()
 
 @app.get('/get_messages/{chat_id}')
 async def get_messages(chat_id: int, count: int = 0):
     entities = {}
     messages = []
 
-    e: Entity = None
-    try:
-        e = await client.get_entity(chat_id)
-    except ValueError:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Entity {chat_id} not found"
-        )
+    e: Entity = get_entity(entity=chat_id, raw=True)
 
     async for message in client.iter_messages(entity=e, reverse=False, limit=count):
         e_id: int = None 
@@ -70,6 +69,7 @@ async def get_messages(chat_id: int, count: int = 0):
 
     return messages
 
+
 @app.get('/get_chats')
 async def get_chats():
     chats = {}
@@ -86,19 +86,24 @@ async def get_chats():
 
     return chats
 
+
 def represents_int(s):
     try: 
         return int(s)
     except ValueError:
         return False
 
+
 @app.get('/get_entity/{entity}')
-async def get_entity(entity: int|str):
+async def get_entity(entity: int|str, raw: bool = False):
     if i := represents_int(entity):
         entity = i
 
     try:
         e_obj = await client.get_entity(entity)
+        if raw:
+            return e_obj
+
         return await format_entity(e_obj)
     except ValueError:
         raise HTTPException(
@@ -106,24 +111,19 @@ async def get_entity(entity: int|str):
             detail=f"Entity {entity} not found"
         )
 
-class PostMessage(BaseModel):
+
+class SendMessagePost(BaseModel):
     entity: int|str
     content: str
     reply_message_id: int = None
 
-@app.post('/send_message')
-async def send_message(post_msg: PostMessage):
-    if i := represents_int(post_msg.entity):
-        post_msg.entity = i
 
-    e_obj = None
-    try:
-        e_obj = await client.get_entity(post_msg.entity)
-    except ValueError:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Entity {post_msg.entity} not found"
-        )
+@app.post('/send_message')
+async def send_message(post_msg: SendMessagePost):
+    e_obj = get_entity(
+        entity=post_msg.entity,
+        raw=True
+    )
     
     message = await client.send_message(
         entity=e_obj,
@@ -136,5 +136,6 @@ async def send_message(post_msg: PostMessage):
         return {'message_id': message.id}
     else:
         raise HTTPException(status_code=404, detail="Message not found")
+
 
 # TODO: handle folders
